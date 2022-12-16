46b62b704c5f1f3f80cdabb7e2922c8c35936333
Wesley Huang <wezmag@gmail.com>
2022-12-16 10:31:06 +0800

    Remove existing Dashboard and bundled CSS and JS

--- DashboardSample/DashboardSample.Web/Modules/Common/Dashboard/DashboardPage.cs
+++ DashboardSample/DashboardSample.Web/Modules/Common/Dashboard/DashboardPage.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Serenity;
 using Serenity.Abstractions;
 using Serenity.Data;
@@ -11,38 +11,9 @@ namespace DashboardSample.Common.Pages
     public class DashboardController : Controller
     {
         [PageAuthorize, HttpGet, Route("~/")]
-        public ActionResult Index(
-            [FromServices] ITwoLevelCache cache,
-            [FromServices] ISqlConnections sqlConnections
-            )
+        public ActionResult Index()
         {
-            if (cache is null)
-                throw new ArgumentNullException(nameof(cache));
-
-            if (sqlConnections is null)
-                throw new ArgumentNullException(nameof(sqlConnections));
-
-            var o = Serenity.Demo.Northwind.OrderRow.Fields;
-
-            var cachedModel = cache.GetLocalStoreOnly("DashboardPageModel", TimeSpan.FromMinutes(5),
-                o.GenerationKey, () =>
-                {
-                    var model = new DashboardPageModel();
-                    using (var connection = sqlConnections.NewFor<Serenity.Demo.Northwind.OrderRow>())
-                    {
-                        model.OpenOrders = connection.Count<Serenity.Demo.Northwind.OrderRow>(
-                            o.ShippingState == (int)Serenity.Demo.Northwind.OrderShippingState.NotShipped);
-                        var closedOrders = connection.Count<Serenity.Demo.Northwind.OrderRow>(
-                            o.ShippingState == (int)Serenity.Demo.Northwind.OrderShippingState.Shipped);
-                        var totalOrders = model.OpenOrders + closedOrders;
-                        model.ClosedOrderPercent = (int)Math.Round(totalOrders == 0 ? 100 :
-                            ((double)closedOrders / totalOrders * 100));
-                        model.CustomerCount = connection.Count<Serenity.Demo.Northwind.CustomerRow>();
-                        model.ProductCount = connection.Count<Serenity.Demo.Northwind.ProductRow>();
-                    }
-                    return model;
-                });
-            return View(MVC.Views.Common.Dashboard.DashboardIndex, cachedModel);
+            return View(MVC.Views.Common.Dashboard.DashboardIndex);
         }
     }
 }
